02ce00632c795dea312eb49d4ee533e66ad5ef66
vladsav1980 <vladsav1980@gmail.com>
2023-07-29 20:53:24 +0300

    home upd

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,13 @@ import LandingSection from "./components/LandingSection";
 import ProjectsSection from "./routes/ProjectsSection";
 import ContactMeSection from "./routes/ContactMeSection";
 
-
 function Home() {
-  return (   
-          <LandingSection />
-          <ProjectsSection />
-          <ContactMeSection />          
+  return (
+    <>
+      <LandingSection />
+      <ProjectsSection />
+      <ContactMeSection />
+    </>
   );
 }
 
