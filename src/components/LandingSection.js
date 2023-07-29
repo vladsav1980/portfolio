59ca90062d80f6e1dc99f0a423e7afe7fb52a3f7
vladsav1980 <vladsav1980@gmail.com>
2023-07-29 15:08:15 +0300

    first commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Vlad!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={4}>
+      <Avatar size="xl" src={require("../images/round_me.png")} />
+      <Heading color="white" size="md">
+        {greeting}
+      </Heading>
+      <Heading color="white" size="3xl">
+        {bio1}
+      </Heading>
+      <Heading color="white" size="3xl">
+        {bio2}
+      </Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
