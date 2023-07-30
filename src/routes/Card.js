334559cab4685d13d1bdd5f5d894cfc686463fe5
vladsav1980 <vladsav1980@gmail.com>
2023-07-30 21:43:15 +0300

    minor upd

--- src/routes/Card.js
+++ src/routes/Card.js
@@ -1,4 +1,4 @@
-import { Heading, Image, Text, VStack, Link } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, VStack, Link } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -18,18 +18,19 @@ const Card = ({ title, description, imageSrc, url }) => {
         transition: "transform 0.7s",
       }}
     >
-      <Image src={imageSrc} alt={title} width="100%" borderRadius="md" />
-      <Heading as="h2" size="md" fontWeight="semibold">
-        {title}
-      </Heading>
-      <Text color="#64748b" fontSize="lg">
-        {description}
-      </Text>
-      {url && (
-        <Link href={url} isExternal>
-          Learn more <FontAwesomeIcon icon={faArrowRight} size="1x" />
-        </Link>
-      )}
+      <Link href={url} isExternal>
+        <Image src={imageSrc} alt={title} width="100%" borderRadius="md" />
+        <Heading as="h2" size="md" fontWeight="semibold">
+          {title}
+        </Heading>
+        <Text color="#64748b" fontSize="lg">
+          {description}
+        </Text>
+        <HStack>
+          <Text>Learn more</Text>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </Link>
     </VStack>
   );
 };
